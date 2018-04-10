65f680d75b613f30510f2d5f9a11bf06acea8ab1
dslosky-usgs <dslosky@usgs.gov>
2018-04-10 09:41:04 -0600

    Asyc overlay

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -33,6 +33,7 @@ import { DateTimePipe } from './date-time.pipe';
 import { GetProductPipe } from './get-product.pipe';
 import { RegionInfoOverlaysPipe } from './region-info-overlays.pipe';
 import { InteractiveMapOverlaysPipe } from './interactive-map-overlays.pipe';
+import { ShakemapOverlaysPipe } from './shakemap-overlays.pipe';
 
 
 @NgModule({
@@ -70,7 +71,8 @@ import { InteractiveMapOverlaysPipe } from './interactive-map-overlays.pipe';
     DateTimePipe,
     GetProductPipe,
     InteractiveMapOverlaysPipe,
-    RegionInfoOverlaysPipe
+    RegionInfoOverlaysPipe,
+    ShakemapOverlaysPipe
   ],
   exports: [
     AttributionComponent,
