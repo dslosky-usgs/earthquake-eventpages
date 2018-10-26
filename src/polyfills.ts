ed41ad3a8fb9044c42e8e29796946168e865b5ad
dslosky-usgs <dslosky@usgs.gov>
2018-10-26 11:21:22 -0600

    Added polyfill for IE/Edge to see station popups

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,6 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+/* Polyfill for IE/Edge to use Web Components */
+import '@webcomponents/webcomponentsjs/webcomponents-bundle';
